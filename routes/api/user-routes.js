33af3c3b8b8c8923c2c8a9c5dc8f4cb0f6e545f3
musaliyah <asmusaliar@gmail.com>
2022-10-20 16:06:38 -0700

    user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+const {
+    getAllUser,
+    getUserById,
+    updateUser,
+    createUser,
+    deleteUser,
+    deleteUser,
+    deleteFriend,
+    addFriend
+} = require('../../controllers/user-controller');
+
+router.route('/').get(getAllUser).post(createUser);
+
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/friendId').post(addFriend).delete.apply(deleteFriend);
+
+module.exports = router;
