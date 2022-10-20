e304046ded06778bdbbc4993ba9c85077e591e3f
musaliyah <asmusaliar@gmail.com>
2022-10-20 16:15:16 -0700

    going to opc studyhall next step is createThought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -10,5 +10,29 @@ const thoughtController = {
         .select('-__v')
         .sort({_id: -1})
         .then(dbThoughtData => res.json(dbThoughtData))
-    }
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    getThoughtById({params}, res) {
+        Thought.findOne({_id: params.id}).populate({
+            path: 'reaction',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({_id: -1})
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({message: 'No thoughts found with that id!'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
 }
