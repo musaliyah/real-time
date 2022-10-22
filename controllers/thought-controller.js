0dfa638247c818bf63a4151ff954233f2c114273
musaliyah <asmusaliar@gmail.com>
2022-10-21 17:53:01 -0700

    another commit

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -35,4 +35,37 @@ const thoughtController = {
             res.sendStatus(400);
         });
     },
+    createThought({body}, res) {
+        Thought.create(body).then(({_id}) => {
+            return User.findOneAndUpdate(
+                {_id: body.userId},
+                {$push: {thoughts: _id}},
+                {new: true}
+            );
+        }).then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({message:'No user in our system matches this id'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+    },
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({_id: params.id}, body, { new: true, runValidators: true}).then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({message: 'No thoughts found with that id!'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+    },
+    updateThought({params, body}, res) {
+        Thought.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true}).then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({message: 'No thoughts in our system match that id'});
+                return;
+            }
+        })
 }
